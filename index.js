c6c2c23cd4831e5d6436b77dd3a4051ef651e80d
anhnguyen3275 <anhnguyen3275@gmail.com>
2021-07-16 10:33:11 +0700

    message

--- index.js
+++ index.js
@@ -1,6 +1,110 @@
 const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
+const { query } = require('express');
+const { base64encode, base64decode } = require('nodejs-base64');
+const { encode } = require('js-base64');
+
+
+var Base64 = {
+  // private property
+  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+  // public method for encoding
+  encode: function (input) {
+    var output = "";
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+    var i = 0;
+    input = Base64._utf8_encode(input);
+    while (i < input.length) {
+      chr1 = input.charCodeAt(i++);
+      chr2 = input.charCodeAt(i++);
+      chr3 = input.charCodeAt(i++);
+      enc1 = chr1 >> 2;
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+      enc4 = chr3 & 63;
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64;
+      } else if (isNaN(chr3)) {
+        enc4 = 64;
+      }
+      output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+    }
+    return output;
+  },
+  // public method for decoding
+  decode: function (input) {
+    var output = "";
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0;
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    while (i < input.length) {
+      enc1 = this._keyStr.indexOf(input.charAt(i++));
+      enc2 = this._keyStr.indexOf(input.charAt(i++));
+      enc3 = this._keyStr.indexOf(input.charAt(i++));
+      enc4 = this._keyStr.indexOf(input.charAt(i++));
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+      output = output + String.fromCharCode(chr1);
+      if (enc3 != 64) {
+        output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+        output = output + String.fromCharCode(chr3);
+      }
+    }
+    output = Base64._utf8_decode(output);
+    return output;
+  },
+  // private method for UTF-8 encoding
+  _utf8_encode: function (string) {
+    string = string.replace(/\r\n/g, "\n");
+    var utftext = "";
+    for (var n = 0; n < string.length; n++) {
+      var c = string.charCodeAt(n);
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      }
+      else if ((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    }
+    return utftext;
+  },
+  // private method for UTF-8 decoding
+  _utf8_decode: function (utftext) {
+    var string = "";
+    var i = 0;
+    var c = c1 = c2 = 0;
+    while (i < utftext.length) {
+      c = utftext.charCodeAt(i);
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      }
+      else if ((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i + 1);
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        i += 2;
+      }
+      else {
+        c2 = utftext.charCodeAt(i + 1);
+        c3 = utftext.charCodeAt(i + 2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        i += 3;
+      }
+    }
+    return string;
+  }
+}
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
@@ -13,7 +117,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), listLabels);
+  authorize(JSON.parse(content), listMessages);
 });
 
 /**
@@ -89,8 +193,6 @@ function listLabels(auth) {
     }
   });
 
-
-  // còn gọi hàm này ra rồi truyền auth vao thôi , em xem nõ chóy main chô nao thi check nhe
 }
 
 function listMessages(auth, query) {
@@ -118,6 +220,7 @@ function listMessages(auth, query) {
   })
 }
 
+
 function getMail(msgId, auth) {
   console.log(msgId)
   const gmail = google.gmail({ version: 'v1', auth });
@@ -126,50 +229,51 @@ function getMail(msgId, auth) {
     userId: 'me',
     id: msgId,
   }, (err, res) => {
-    // lam gi co labelIds ma get ra
     console.log(res.data.labelIds.INBOX)
     if (!err) {
-      console.log("no error")
-      var body = res.data.payload.parts[0].body.data;
+      console.log("no error");
+      // console.log("payload", res.data.payload.parts[0].parts[1].body.data);
+      var body = res.data.payload.parts[0].parts[1].body.data;
 
-      // var htmlBody = base64.decode(body.replace(/-/g, '+').replace(/_/g, '/'));
-      console.log(res.data.snippet)
-      // var mailparser = new Mailparser();
+      var htmlBody = Base64.decode(body.replace(/-/g, '+').replace(/_/g, '/'));
+      console.log(htmlBody)
+      var mailparser = new Mailparser();
 
-      // mailparser.on("end", (err,res) => {
-      //     console.log("res",res);
-      // })
+      mailparser.on("end", (err, res) => {
+        console.log("res", res);
+      })
 
-      // mailparser.on('data', (dat) => {
-      //     if(dat.type === 'text'){
-      //         const $ = cheerio.load(dat.textAsHtml);
-      //         var links = [];
-      //         var modLinks = [];
-      //         $('a').each(function(i) {
-      //             links[i] = $(this).attr('href');
-      //         });
+      mailparser.on('data', (dat) => {
+        if (dat.type === 'text') {
+          const $ = cheerio.load(dat.textAsHtml);
+          var links = [];
+          var modLinks = [];
+          $('a').each(function (i) {
+            links[i] = $(this).attr('href');
+          });
 
-      //         //Regular Expression to filter out an array of urls.
-      //         var pat = /------[0-9]-[0-9][0-9]/;
+          //Regular Expression to filter out an array of urls.
+          var pat = /------[0-9]-[0-9][0-9]/;
 
-      //         //A new array modLinks is created which stores the urls.
-      //         modLinks = links.filter(li => {
-      //             if(li.match(pat) !== null){
-      //                 return true;
-      //             }
-      //             else{
-      //                 return false;
-      //             }
-      //         });
-      //         console.log(modLinks);
+          //A new array modLinks is created which stores the urls.
+          modLinks = links.filter(li => {
+            if (li.match(pat) !== null) {
+              return true;
+            }
+            else {
+              return false;
+            }
+          });
+          console.log(modLinks);
 
-      //         //This function is called to open all links in the array.
+          //This function is called to open all links in the array.
 
-      //     }
-      // })
+        }
+      })
 
-      // mailparser.write(htmlBody);
-      // mailparser.end();
+      mailparser.write(htmlBody);
+      console.log("END!!!")
+      mailparser.end();
 
     }
   });
